e2076f71c60f70267c563c8f386628d9ae50c96d
Kevin <kevinponting82@gmail.com>
2023-09-22 22:54:52 -0700

    somewhat working

---
+++ Calculator.js
@@ -0,0 +1,55 @@
+class Calculator {
+    constructor(output) {
+        this.output = output
+        this.clear();
+    }
+
+    inputNumber(number) {
+        if (this.equation !== '') {
+            this.equation += number;
+        }
+        else {
+            this.equation = number;
+        }
+
+    }
+
+    clear() {
+        this.equation = "";
+        this.operation = undefined;
+    }
+
+    operationChoice(operation) {
+
+    }
+
+    calculate() {
+
+    }
+
+    update() {
+        this.output.innerText = this.equation
+    }
+}
+
+const buttonForNumber = document.querySelectorAll('[data-number]');
+const buttonForOperator = document.querySelectorAll('[data-operation]');
+const buttonForEqual = document.querySelector('[data-equal]');
+const buttonForClear = document.querySelector('[data-clear]');
+const output = document.querySelector('[data-output]');
+
+const calculator = new Calculator(output);
+
+buttonForNumber.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.inputNumber(button.innerText);
+        calculator.update();
+    })
+})
+
+buttonForClear.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.clear();
+        calculator.update();
+    })
+})
