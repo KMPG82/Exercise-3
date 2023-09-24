8dbae2da86dfa9fadcf6a8c1ea63e13040ac1c47
Kevin <kevinponting82@gmail.com>
2023-09-23 20:34:48 -0700

    can add two numbers

--- Calculator.js
+++ Calculator.js
@@ -1,63 +1,52 @@
-class Calculator {
-    constructor(output) {
-        this.output = output
-        this.clear();
-    }
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatornButtons = document.querySelectorAll('[data-operator]')
+const display = document.querySelector('.display')
+const equalButton = document.querySelector('[data-equal]')
 
-    inputNumber(number) {
-        if (this.equation !== '') {
-            //might need to cast both as a string then concatanate
-            this.equation += number;
+
+let operand = '';
+let inputNumbers = []; // organize input numbers
+let inputOperators = []; //organize input operators
+let operators = ['+', '-', 'x', '/'];
+
+function addNumber(number) {
+    display.innerText += number;
+    operand += number;
+    console.log(operand)
+}
+
+function addOperator(operator) {
+    display.innerText += operator;
+    inputNumbers.push(operand)
+    operand = ''
+    inputOperators.push(operator)
+    console.log(inputNumbers)
+    console.log(inputOperators)
+}
+
+function calculate() {
+    inputNumbers.push(operand)
+    inputNumbers.reverse()
+    let ouput = 0;
+
+    while (inputOperators.length !== 0) {
+        if (inputOperators[0] == '+') {
+            output = Number(inputNumbers[inputNumbers.length - 2]) + Number(inputNumbers[inputNumbers.length - 1]);
+            inputOperators.pop();
+            inputNumbers.pop()
+            inputNumbers.pop()
+
+            display.innerText = output
         }
-        else {
-            this.equation = number;
-        }
-
-    }
-
-    clear() {
-        this.equation = "";
-        this.operation = undefined;
-    }
-
-    operationChoice(operation) {
-
-    }
-
-    calculate() {
-
-    }
-
-    update() {
-        this.output.innerText = this.equation
     }
 }
 
-const buttonForNumber = document.querySelectorAll('[data-number]');
-const buttonForOperator = document.querySelectorAll('[data-operation]');
-const buttonForEqual = document.querySelector('[data-equal]');
-const buttonForClear = document.querySelector('[data-clear]');
-const output = document.querySelector('[data-output]');
-
-const calculator = new Calculator(output);
-
-buttonForNumber.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.inputNumber(button.innerText);
-        calculator.update();
-    })
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => addNumber(button.innerText));
 })
 
-buttonForOperator.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.operationChoice(button.innerText);
-        calculator.update();
-    })
+operatornButtons.forEach(button => {
+    button.addEventListener('click', () => addOperator(button.innerText));
 })
 
-buttonForClear.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.clear();
-        calculator.update();
-    })
-})
+equalButton.addEventListener('click', () => calculate());
